219dcf790429bc12a497e167f80fa9bf06eb35a7
Michael Rawson <michael@rawsons.uk>
2020-10-27 15:50:52 +0000

    demonstration of tracebacks

--- Saturation/SaturationAlgorithm.cpp
+++ Saturation/SaturationAlgorithm.cpp
@@ -401,6 +401,7 @@ void SaturationAlgorithm::onAllProcessed()
  */
 void SaturationAlgorithm::onPassiveAdded(Clause* c)
 {
+  throw;
   if (env.options->showPassive()) {
     env.beginOutput();
     env.out() << "[SA] passive: " << c->toString() << std::endl;
