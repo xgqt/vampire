dd87c159e5e4f522b8932f33283821c0126197a0
Michael Rawson <michael@rawsons.uk>
2022-10-19 15:16:34 +0100

    subterm relations

--- Kernel/Theory.cpp
+++ Kernel/Theory.cpp
@@ -740,8 +740,6 @@ unsigned Theory::getArity(Interpretation i)
   case REAL_LESS:
   case REAL_LESS_EQUAL:
 
-  case SUBTERM:
-
   case INT_PLUS:
   case INT_MINUS:
   case INT_MULTIPLY:
@@ -780,6 +778,7 @@ unsigned Theory::getArity(Interpretation i)
     return 2;
           
   case ARRAY_STORE:
+  case SUBTERM:
 
     return 3;
           
